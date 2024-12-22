4b43dbd22ea4a40ee604d9a3532e534b2c6e923c
Ted Shaffer <shaffer.family@gmail.com>
2024-12-22 04:45:11 -0800

    Agent only supports Tavily

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -2,13 +2,6 @@ import dotenv from 'dotenv';
 import { ChatOpenAI } from "@langchain/openai";
 import { ChatPromptTemplate } from "@langchain/core/prompts";
 import { TavilySearchResults } from "@langchain/community/tools/tavily_search";
-import "cheerio"; // This is required in notebooks to use the `CheerioWebBaseLoader`
-import { CheerioWebBaseLoader } from "@langchain/community/document_loaders/web/cheerio";
-import { MemoryVectorStore } from "langchain/vectorstores/memory";
-import { OpenAIEmbeddings } from "@langchain/openai";
-import { RecursiveCharacterTextSplitter } from "@langchain/textsplitters";
-import { z } from "zod";
-import { tool } from "@langchain/core/tools";
 import { createToolCallingAgent } from "langchain/agents";
 import { AgentExecutor } from "langchain/agents";
 import { ChainValues } from '@langchain/core/utils/types';
@@ -22,69 +15,9 @@ console.log(process.env.TAVILY_API_KEY);
 const search = new TavilySearchResults({
   maxResults: 2,
 });
-// const searchResults: any = await search.invoke("what is the weather in SF");
-// console.log('searchResults:', searchResults);
-
-const loader = new CheerioWebBaseLoader(
-  "https://docs.smith.langchain.com/overview"
-);
-const docs = await loader.load();
-const splitter = new RecursiveCharacterTextSplitter({
-  chunkSize: 1000,
-  chunkOverlap: 200,
-});
-const documents = await splitter.splitDocuments(docs);
-const vectorStore = await MemoryVectorStore.fromDocuments(
-  documents,
-  new OpenAIEmbeddings()
-);
-const retriever = vectorStore.asRetriever();
-
-// const retrieverResponse: any = (await retriever.invoke("how to upload a dataset"))[0];
-// console.log('retrieverResponse:', retrieverResponse);
-
-const retrieverTool = tool(
-  async ({ input }, config) => {
-    const docs = await retriever.invoke(input, config);
-    return docs.map((doc) => doc.pageContent).join("\n\n");
-  },
-  {
-    name: "langsmith_search",
-    description:
-      "Search for information about LangSmith. For any questions about LangSmith, you must use this tool!",
-    schema: z.object({
-      input: z.string(),
-    }),
-  }
-);
-
-const tools = [search, retrieverTool];
+const tools = [search];
 
 const model = new ChatOpenAI({ model: "gpt-4" });
-
-// const modelWithTools = model.bindTools(tools);
-
-// const responseWithTools = await modelWithTools.invoke([
-//   {
-//     role: "user",
-//     content: "Hi!",
-//   },
-// ]);
-
-// console.log(`Content: ${responseWithTools.content}`);
-// console.log(`Tool calls: ${responseWithTools.tool_calls}`);
-
-// const responseWithToolCalls = await modelWithTools.invoke([
-//   {
-//     role: "user",
-//     content: "What's the weather in SF?",
-//   },
-// ]);
-
-// console.log(`Content: ${responseWithToolCalls.content}`);
-// console.log(
-//   `Tool calls: ${JSON.stringify(responseWithToolCalls.tool_calls, null, 2)}`
-// );
 
 const prompt = ChatPromptTemplate.fromMessages([
   ["system", "You are a helpful assistant"],
@@ -102,16 +35,8 @@ const agentExecutor = new AgentExecutor({
   tools,
 });
 
-// const agentResults: ChainValues = await agentExecutor.invoke({ input: "hi!" });
-
-// const agentResults: ChainValues = await agentExecutor.invoke({ input: "how can langsmith help with testing?" });
-
 const agentResults: ChainValues = await agentExecutor.invoke({ input: "what can you tell me about the restaurant Capelo's Barbecue in Redwood City, California?" });
 
-
 console.log(agentResults);
-
-
-
 
 console.log('exit');
